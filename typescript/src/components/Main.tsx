12cec00ea77d6e71f53364ce533cfccbe0607008
K1ngHandy <bolds-slicks0g@icloud.com>
2025-03-13 20:22:38 -0400

    never, undefined, and null

--- typescript/src/components/Main.tsx
+++ typescript/src/components/Main.tsx
@@ -12,6 +12,29 @@ function Main() {
     let str: any = true;
     str = "string" // no error as "any" type
     Math.round(str); // no error as "any" type
+    
+    // TypeScript will prevent unknown from being used
+    let w: unknown = 1;
+    w = "string" // no error
+    w = {
+        runANonExistentMethod: () => {
+            console.log("Here we go.");
+        }
+    } as { runANonExistentMethod: () => void }
+    if (typeof w === 'object' && w !== null) {
+        // run w.runANonExistentMethod(); without error when type is 'unknown'
+        // casting with as
+        (w as { runANonExistentMethod: Function }).runANonExistentMethod();
+    }
+
+    // type never, rarely used
+    // let x: never = true; // Error: type boolean is not assignable to type 'never'
+
+    // undefined and null
+    let y: undefined = undefined;
+    console.log(typeof y);
+    let z: null = null;
+    console.log(typeof z);
 
     return (
         <>
