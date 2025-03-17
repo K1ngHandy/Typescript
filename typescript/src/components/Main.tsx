ae8c6775ffff9f756cda65b893de66fa6e359122
K1ngHandy <bolds-slicks0g@icloud.com>
2025-03-17 19:57:42 -0400

    ts arrays

--- typescript/src/components/Main.tsx
+++ typescript/src/components/Main.tsx
@@ -36,6 +36,24 @@ function Main() {
     let z: null = null;
     console.log(typeof z);
 
+    // arrays
+    const names: string[] = [];
+    names.push("Typer"); // no error
+    console.log('Names:', names);
+    // names.push(3); // error: argument of type 'number' is not assignable to parameter of type 'string'
+
+    // readonly
+    const names2: readonly string[] = ["Typette"];
+    console.log('Names 2:', names2);
+    // names2.push("Jack"); // error: property 'push' does not exist on type 'readonly string[]'
+
+    // type inference
+    const numbers = [1, 2, 3]; // inferred to type number[]
+    numbers.push(4); // no error
+    // numbers.push("2"); // error: argument of type 'string' is not assignable to parameter of type 'number'
+    let head: number = numbers[0]; // no error
+    console.log('Head:', head);
+
     return (
         <>
             <h2 className="first-name">{firstName}</h2>
